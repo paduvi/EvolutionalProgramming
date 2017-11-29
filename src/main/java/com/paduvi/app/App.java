062da71de601799a23c48ad49d1880ba99bc3b99
vietpd <viet3695@gmail.com>
2017-11-29 19:47:51 +0700

    update epoch number

--- src/main/java/com/paduvi/app/App.java
+++ src/main/java/com/paduvi/app/App.java
@@ -5,7 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -19,11 +24,92 @@ import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.paduvi.app.Main.Solution;
 import com.paduvi.app.models.Contractor;
+import com.paduvi.app.models.Pack;
+import com.paduvi.app.models.Product;
 import com.paduvi.app.models.Project;
 import com.paduvi.app.models.Timestamp;
 import com.paduvi.app.models.TimestampDeserializer;
 
 public class App {
+	public static void createNoCell(Row row, AtomicInteger colNum, int value) {
+		Cell cell = row.createCell(colNum.getAndIncrement());
+		cell.setCellValue(value);
+	}
+
+	public static void createBuyerProfitCell(Row row, AtomicInteger colNum, Project project, List<Solution> solutions) {
+		Cell cell = row.createCell(colNum.getAndIncrement());
+
+		double profit = IntStream.range(0, project.getPackages().size()).parallel().mapToDouble(i -> {
+			Contractor contractor = solutions.get(i).getContractor();
+			Timestamp executedDate = solutions.get(i).getExecutedDate();
+			Pack pkg = project.getPackages().get(i);
+			double sumSell = pkg.getProducts().parallelStream().mapToDouble(p -> {
+				int productId = p.getProductId();
+				Product temp = contractor.getProducts().parallelStream().filter(p1 -> p1.getProductId() == productId)
+						.findAny().get();
+				return pkg.getEstimatedCost()
+						- temp.getSellPrice() * (1 - temp.getDiscountRate(executedDate)) * p.getQuantity();
+			}).sum();
+			return sumSell
+					* Math.exp(project.getInflationRate() * (executedDate.get() - project.getStartDate().get()) / 7);
+		}).sum();
+		cell.setCellValue(String.format("%,.2f", profit));
+	}
+
+	public static void createBuyerProfitDiffCell(Row row, AtomicInteger colNum, Project project,
+			List<Solution> solutions, List<Contractor> contractors) {
+		Cell cell = row.createCell(colNum.getAndIncrement());
+		double[] listRevenues = new double[contractors.size()];
+		IntStream.range(0, project.getPackages().size()).parallel().forEach(i -> {
+			Contractor contractor = solutions.get(i).getContractor();
+			Timestamp executedDate = solutions.get(i).getExecutedDate();
+			Pack pkg = project.getPackages().get(i);
+			double differentPrice = pkg.getProducts().parallelStream().mapToDouble(p -> {
+				int productId = p.getProductId();
+				Product temp = contractor.getProducts().parallelStream().filter(p1 -> p1.getProductId() == productId)
+						.findAny().get();
+				return (temp.getSellPrice() * (1 - temp.getDiscountRate(executedDate)) - temp.getBuyPrice())
+						* p.getQuantity();
+			}).sum();
+			listRevenues[contractor.getContractorId()] += differentPrice
+					* Math.exp(project.getInflationRate() * (executedDate.get() - project.getStartDate().get()) / 7);
+		});
+
+		double sum = 0;
+		for (int i = 0; i < contractors.size() - 1; i++) {
+			for (int j = i; j < contractors.size(); j++) {
+				sum += Math.abs(contractors.get(i).getRelationship() * listRevenues[i]
+						- contractors.get(j).getRelationship() * listRevenues[j]);
+			}
+		}
+
+		cell.setCellValue(String.format("%,.2f", sum));
+	}
+
+	public static void createQualityProfitCell(Row row, AtomicInteger colNum, List<Solution> solutions) {
+		Cell cell = row.createCell(colNum.getAndIncrement());
+		double sumQuality = solutions.parallelStream().mapToDouble(solution -> solution.getContractor().getQuality())
+				.sum();
+		cell.setCellValue(sumQuality);
+	}
+
+	public static void createPackageProfitCell(Row row, AtomicInteger colNum, Solution solution, Pack pkg,
+			Project project) {
+		Cell cell = row.createCell(colNum.getAndIncrement());
+
+		Contractor contractor = solution.getContractor();
+		Timestamp executedDate = solution.getExecutedDate();
+		double differentPrice = pkg.getProducts().parallelStream().mapToDouble(p -> {
+			int productId = p.getProductId();
+			Product temp = contractor.getProducts().parallelStream().filter(p1 -> p1.getProductId() == productId)
+					.findAny().get();
+			return (temp.getSellPrice() * (1 - temp.getDiscountRate(executedDate)) - temp.getBuyPrice())
+					* p.getQuantity();
+		}).sum();
+		cell.setCellValue(String.format("%,.2f", differentPrice
+				* Math.exp(project.getInflationRate() * (executedDate.get() - project.getStartDate().get()) / 7)));
+	}
+
 	public static void main(String[] args) throws Exception {
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
@@ -40,11 +126,12 @@ public class App {
 
 		XSSFWorkbook workbook = new XSSFWorkbook();
 		XSSFSheet sheet = workbook.createSheet("NSGA-II Report");
-		String[] headers = new String[project.getPackages().size() * 2 + 1];
-		headers[0] = "STT";
+		List<String> headers = new ArrayList<String>();
+		Collections.addAll(headers, "STT", "Buyer Profit", "Provider Profit Diff", "Quality");
 		for (int i = 0; i < project.getPackages().size(); i++) {
-			headers[1 + 2 * i] = "Package " + i + " Provider";
-			headers[2 * (i + 1)] = "Package " + i + " Date";
+			headers.add("Package " + i + " Provider");
+			headers.add("Package " + i + " Date");
+			headers.add("Package " + i + " Profit");
 		}
 
 		int rowNum = 0;
@@ -56,20 +143,35 @@ public class App {
 			cell.setCellValue(header);
 		}
 
-		for (int i = 0; i < 10; i++) {
+		sheet.createFreezePane(0, 1);
+
+		for (int i = 0; i < 40; i++) {
 			Main app = new Main(project, contractors);
 			List<Solution> solutions = app.getSolution();
 			app.printResult();
 			row = sheet.createRow(rowNum++);
-			Cell cell = row.createCell(0);
-			cell.setCellValue(i + 1);
-			colNum = 1;
+
+			AtomicInteger colNo = new AtomicInteger(0);
+			createNoCell(row, colNo, i + 1);
+			createBuyerProfitCell(row, colNo, project, solutions);
+			createBuyerProfitDiffCell(row, colNo, project, solutions, contractors);
+			createQualityProfitCell(row, colNo, solutions);
+
 			for (int j = 0; j < solutions.size(); j++) {
-				cell = row.createCell(colNum++);
+				Cell cell = row.createCell(colNo.getAndIncrement());
 				cell.setCellValue(solutions.get(j).getContractor().getContractorId());
-				cell = row.createCell(colNum++);
+				cell = row.createCell(colNo.getAndIncrement());
 				cell.setCellValue(solutions.get(j).getExecutedDate().toString());
+				createPackageProfitCell(row, colNo, solutions.get(j), project.getPackages().get(j), project);
 			}
+		}
+
+		row = sheet.getRow(0);
+		Iterator<Cell> cellIterator = row.cellIterator();
+		while (cellIterator.hasNext()) {
+			Cell cell = cellIterator.next();
+			int columnIndex = cell.getColumnIndex();
+			sheet.autoSizeColumn(columnIndex);
 		}
 
 		try {
